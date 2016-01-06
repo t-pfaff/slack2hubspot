bad43d475b96c4aadc7947f9e90915899dd90772
Tobias Pfaff <tobias@datalook.io>
2016-01-06 23:14:58 +0100

    add port to app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@
 # encoding: utf-8
 
 from bottle import run, post
+import os
 
 @post('/contact')
 def contact():
     return 'Hello World!'
+
+port = int(os.environ.get('PORT', 5000))
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=5000)
